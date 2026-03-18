5ca395b293dcfb6ecf80a9f9b5aefeaa1c05e868
CyberSphinxx <johnlemargonzales@gmail.com>
2026-03-18 18:13:00 +0800

    chore: update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [react()],
 });
